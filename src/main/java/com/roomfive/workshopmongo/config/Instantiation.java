398fd64af13155beca16db7230099815df3ec960
Samuel Bento <samuel94bento@gmail.com>
2019-09-25 18:27:48 +0100

    Acrescentando comentários aos posts

--- src/main/java/com/roomfive/workshopmongo/config/Instantiation.java
+++ src/main/java/com/roomfive/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.roomfive.workshopmongo.domain.Post;
 import com.roomfive.workshopmongo.domain.User;
 import com.roomfive.workshopmongo.dto.AuthorDTO;
+import com.roomfive.workshopmongo.dto.CommentDTO;
 import com.roomfive.workshopmongo.repository.PostRepository;
 import com.roomfive.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para S.Paulo. Abraços!!",
 				new AuthorDTO(maria));
 		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveita", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post.getComments().addAll(Arrays.asList(c1,c2));
+		post1.getComments().addAll(Arrays.asList(c3));
 
 		postRepository.saveAll(Arrays.asList(post, post1));
 
