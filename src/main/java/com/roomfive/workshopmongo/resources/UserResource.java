2110dc1e4f114ba397e6b3073e853f6d1f351005
Samuel Bento <samuel94bento@gmail.com>
2019-09-25 15:21:41 +0100

    Inserção de usuário com POST

--- src/main/java/com/roomfive/workshopmongo/resources/UserResource.java
+++ src/main/java/com/roomfive/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.roomfive.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.roomfive.workshopmongo.domain.User;
 import com.roomfive.workshopmongo.dto.UserDTO;
@@ -37,6 +40,15 @@ public class UserResource {
 		UserDTO userDTO = new UserDTO(user);
 
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+
+		User user = service.fromDTO(objDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/roomfive/workshopmongo/services/UserService.java
+++ src/main/java/com/roomfive/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.roomfive.workshopmongo.domain.User;
+import com.roomfive.workshopmongo.dto.UserDTO;
 import com.roomfive.workshopmongo.repository.UserRepository;
 import com.roomfive.workshopmongo.services.exeption.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(()-> new ObjectNotFoundException("Obejeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
