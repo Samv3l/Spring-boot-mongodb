062edf40deb8cbe778c0bcd5c67237dfcdb62bdd
Samuel Bento <samuel94bento@gmail.com>
2019-09-25 17:55:32 +0100

    Referenciando os posts do usuário

--- src/main/java/com/roomfive/workshopmongo/config/Instantiation.java
+++ src/main/java/com/roomfive/workshopmongo/config/Instantiation.java
@@ -19,29 +19,32 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		
-		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para S.Paulo. Abraços!!", new AuthorDTO(maria));
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para S.Paulo. Abraços!!",
+				new AuthorDTO(maria));
 		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-			
-		postRepository.saveAll(Arrays.asList(post,post1));
-		
+
+		postRepository.saveAll(Arrays.asList(post, post1));
+
+		maria.getPosts().addAll(Arrays.asList(post, post1));
+		userRepository.save(maria);
 	}
 
 }
